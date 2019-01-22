5b78fb3ef5f3c7d460fc0ac4b52ba9c16bfc09c8
sanchezla <sanchezmillanluis@gmail.com>
2019-01-22 16:59:11 -0300

    Se importo personas.json en el componente cv

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,8 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
-  public personas;
+
   constructor() {
-    this.personas = require('../assets/personas.json');
-    console.log(this.personas);
     // this.personas = require('../assets/personas.json');
     // console.log(this.personas);
    }
--- src/app/cv/cv.component.ts
+++ src/app/cv/cv.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import personas from '../../assets/personas.json';
+
 
 @Component({
   selector: 'app-cv',
@@ -7,12 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CvComponent implements OnInit {
 
+  public personas: any;
+  public json: any;
+  public id: any;
 
   constructor() {
-
   }
 
   ngOnInit() {
+    this.personas = personas;
+    console.log(JSON.stringify(personas));
   }
+
+
+
+  public seleccionarPersonas ($event) {
+       console.log($event);
+       this.json = this.personas[this.id];
+    }
 
 }

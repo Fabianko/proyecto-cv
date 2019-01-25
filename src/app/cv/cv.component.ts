63a354fc721b540d1f905a4503ed9ef33ff1cda3
sanchezla <sanchezmillanluis@gmail.com>
2019-01-25 19:48:01 -0300

    Se agregó nuevo archivo especialidades.json para seleccionar la especialidad de cada persona y se edito archivo .gitignore

--- src/app/cv/cv.component.ts
+++ src/app/cv/cv.component.ts
@@ -1,6 +1,7 @@
 // declare var require: any;
 import { Component, OnInit } from '@angular/core';
 import personas from '../../assets/json/personas.json';
+import especialidades from '../../assets/json/especialidades.json';
 
 
 @Component({
@@ -12,18 +13,28 @@ export class CvComponent implements OnInit {
 //  imgname = require('../../assets/image/foto.jpg');
   public personas: any;
   public json: any;
-  public id: any;
+
+  // Valor inicial de id para tomar valor del primer optiom
+  public id: any = '';
+  public especialidad: any = '';
+
 
   constructor() {
   }
 
   ngOnInit() {
     this.personas = personas;
+    this.especialidad =  especialidades;
   }
 
   public seleccionarPersonas ($event) {
        console.log($event);
        this.json = this.personas[this.id];
     }
+
+    public seleccionarEspecialidad ($event) {
+         console.log($event);
+         this.json = this.especialidad[this.especialidad];
+      }
 
 }
